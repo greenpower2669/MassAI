2e2cf2b242a8e76337f09deac6c63804faf13e89
greenpower2669 <80179295+greenpower2669@users.noreply.github.com>
2026-09-18 17:44:43 +0200

    Bump MassAI to 0.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "fr.massai.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.6.0"
+        versionCode = 7
+        versionName = "0.7.0"
     }
 
     buildTypes {
